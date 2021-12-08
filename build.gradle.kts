947298f49e7b4eea64583b67fc9924ffbe8659c0
yujin <84204420+euphorxa@users.noreply.github.com>
2021-12-08 12:16:58 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    val kotlinVersion = "1.6.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    val detektVersion = "1.18.0"
+    id("io.gitlab.arturbosch.detekt") version detektVersion
+    application
+}
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+
+    val kordVersion = "0.8.0-M8"
+    val slf4jVersion = "1.7.32"
+    val ktorVersion = "1.6.7"
+
+    val junitVersion = "5.8.2"
+
+    implementation("dev.kord", "kord-core", kordVersion)
+    implementation("org.slf4j", "slf4j-api", slf4jVersion)
+    implementation("org.slf4j", "slf4j-jdk17", slf4jVersion)
+
+    implementation("io.ktor", "ktor-server-core", ktorVersion)
+    implementation("io.ktor", "ktor-server-cio", ktorVersion)
+    implementation("io.ktor", "ktor-serialization", ktorVersion)
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+
+    reports {
+        html.enabled = true
+        xml.enabled = true
+        txt.enabled = true
+        sarif.enabled = true
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("de.gianttree.discord.w2g.WatchTogetherKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>() {
+    jvmTarget = "1.17"
+}
