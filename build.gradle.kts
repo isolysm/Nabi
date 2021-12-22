3dd2c14b657e611622fdcbc73c5d20f2b395f1a3
yujin <84204420+euphorxa@users.noreply.github.com>
2021-12-21 18:44:13 -0800

    Nevermind I am very stupid do not mind me

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,57 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
-    application
+    id("org.jmailen.kotlinter") version "3.8.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
-group = "endeavor.nabi"
-version = "1.0.0"
+group = "in.technowolf"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
-    jcenter() /* BlueUtils & kordx.emoji */
-    maven("https://jitpack.io")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven {
+        name = "nabi"
+        url = uri("https://maven.kotlindiscord.com/repository/maven-public/")
+    }
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
-    //Kord
-    implementation("dev.kord:kord-core:0.8.0-M5")
-    implementation("com.gitlab.kordlib:kordx.emoji:0.4.0")
-
-    //Config
-    implementation("io.github.config4k:config4k:0.4.2")
-    implementation("com.google.code.gson:gson:2.8.6")
-
-    //Logging
-    implementation("org.slf4j:slf4j-simple:1.7.30")
-
-    //Utils
-    implementation("org.apache.commons:commons-lang3:3.11")
-    implementation("net.axay:BlueUtils:1.0.2")
-
-    // Kotlin
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("io.ktor:ktor-client-core:1.6.1")
-    implementation("io.ktor:ktor-client-cio:1.6.1")
-    implementation("io.ktor:ktor-client-serialization:1.6.1")
-    implementation("io.ktor:ktor-client-gson:1.6.1")
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-
-    // MongoDB
-    implementation("org.litote.kmongo", "kmongo-core", "4.2.3")
-    implementation("org.litote.kmongo", "kmongo-serialization-mapping", "4.2.3")
-
-    //PerspectiveAPI
-    implementation("com.github.origma:Google-PerspectiveAPI-Java-Client:0.0.5")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.1-RC1")
+    implementation("com.kotlindiscord.kord.extensions:time4j:1.5.1-RC1")
+    implementation("io.ktor:ktor-client-core:1.6.7")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
+    implementation("net.dean.jraw:JRAW:1.1.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.36.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.36.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.36.1")
+    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
+    implementation ("io.getunleash:unleash-client-java:5.0.3")
+    implementation("dev.kord.x:emoji:0.5.0")
 }
 
-application {
-    mainClass.set("me.obsilabor.noriskclientbot.ManagerKt")
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
 
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>{
-        kotlinOptions.jvmTarget = "17"
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
     }
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
-java.targetCompatibility = JavaVersion.VERSION_17
+tasks.check {
+    dependsOn("installKotlinterPrePushHook")
+}
+
+kotlinter {
+    ignoreFailures = false
+    indentSize = 4
+    reporters = arrayOf("checkstyle", "plain")
+    experimentalRules = false
+    disabledRules = arrayOf("no-wildcard-imports", "import-ordering", "indent")
+}
